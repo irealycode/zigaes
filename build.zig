574325de59f713abde076215911a47517d97c5b4
irealycode <irealycode@localhost.localdomain>
2025-01-31 12:18:06 +0100

    dependency initilization

---
+++ build.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+const Builder = std.Build;
+
+pub fn build(b: *Builder) void {
+    const lib = b.addStaticLibrary(.{
+        .name = "zigaes",
+        .root_source_file = b.path("src/AES.zig"),
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
+    });
+
+    // b.installArtifact(lib);
+    lib.install();
+}
