ce499fa38970f5cbd86331339a247789dd3caa08
irealycode <irealycode@localhost.localdomain>
2025-01-31 12:59:11 +0100

    bug fixes

--- build.zig
+++ build.zig
@@ -2,13 +2,21 @@ const std = @import("std");
 const Builder = std.Build;
 
 pub fn build(b: *Builder) void {
-    const lib = b.addStaticLibrary(.{
-        .name = "zigaes",
-        .root_source_file = b.path("src/AES.zig"),
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    // const lib = b.addStaticLibrary(.{
+    //     .name = "zigaes",
+    //     .root_source_file = b.path("src/AES.zig"),
+    //     .target = b.standardTargetOptions(.{}),
+    //     .optimize = b.standardOptimizeOption(.{}),
+    // });
+
+    const dep_opts = .{ .target = target, .optimize = optimize };
+    _ = dep_opts;
+    _ = b.addModule("zigaes", .{
+        .root_source_file = b.path("src/AES.zig")
     });
 
-    b.installArtifact(lib);
+    // b.installArtifact(lib);
     // lib.install();
 }
